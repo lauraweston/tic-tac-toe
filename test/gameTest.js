263f93a3380649db018ecd28f80df979c91cf08b
Laura Weston <laura.weston@outlook.com>
2016-12-04 17:33:12 +0000

    Reorganise tests into describe blocks

--- test/gameTest.js
+++ test/gameTest.js
@@ -13,35 +13,48 @@ describe("Game", function () {
     it("should start with an empty grid", function () {
         const positions = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
         positions.forEach(function (position) {
-            assert.equal("", game.tokenAt(position));
+            assert.equal(game.tokenAt(position), "");
         });
     });
 
-    it("starts with player 1 move", function () {
-        game.move("a1");
-        assert.equal("O", game.tokenAt("a1"));
+    describe(".move", function () {
+        it("starts with player 1 move", function () {
+            game.move("a1");
+            assert.equal(game.tokenAt("a1"), "O");
+        });
+
+        it("should alternate two players moves", function () {
+            game.move("a1");
+            game.move("b2");
+            game.move("c3");
+            assert.equal(game.tokenAt("b2"), "X");
+            assert.equal(game.tokenAt("c3"), "O");
+        });
+        it("should not allow a player to take a position that is already taken", function () {
+            game.move("a1");
+            game.move("a1");
+            assert.equal(game.tokenAt("a1"), "O");
+        });
+        it("should not change to next player if current player selects a position that is already taken", function () {
+            game.move("a1");
+            game.move("a1");
+            assert.equal(game.turnCount, 1);
+        });
     });
 
-    it("should alternate two players moves", function () {
-        game.move("a1");
-        game.move("b2");
-        game.move("c3");
-        assert.equal("X", game.tokenAt("b2"));
-        assert.equal("O", game.tokenAt("c3"));
-    });
-    it("should not allow a player to take a position that is already taken", function () {
-        game.move("a1");
-        game.move("a1");
-        assert.equal("O", game.tokenAt("a1"));
-    });
-    it("should not change to next player if current player makes an invalid move", function () {
-        game.move("a1");
-        game.move("a1");
-        assert.equal(1, game.turnCount);
-    });
-    it("should declare the game over after all positions have been taken", function () {
-        game.turnCount = 9;
-        assert.equal(true, game.isOver());
+    describe("Game over", function () {
+        it("should declare the game over after all positions have been taken", function () {
+            game.turnCount = 9;
+            assert.equal(game.isOver(), true);
+        });
+        it("should not have a winner if all positions are taken and no player has 3 in a row", function () {
+            const moves = ["a1", "a2", "a3", "b1", "b3", "b2", "c1", "c3", "c2"];
+            moves.forEach(function (position) {
+                game.move(position);
+            });
+            assert.equal(game.isWon(), false);
+            assert.equal(game.isOver(), true);
+        });
     });
 
     describe("Winning moves", function () {
